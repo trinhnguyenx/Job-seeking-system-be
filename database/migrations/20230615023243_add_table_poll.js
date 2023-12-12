bb5056e84dc9d78ca4c07d60138bee973bd0e8f8
CongTrinhh <nguyencongtrinhqb@gmail.com>
2023-12-13 00:13:19 +0700

    update db

--- database/migrations/20230615023243_add_table_poll.js
+++ database/migrations/20230615023243_add_table_poll.js
@@ -16,6 +16,30 @@ exports.up = function(knex) {
 		table.timestamp('created_at').defaultTo(knex.fn.now());
 		table.timestamp('updated_at').defaultTo(knex.fn.now());
 	})
+	.createTable('job_detail', function (table) {
+		table.increments('id').primary();
+		table.string('Title');
+		table.string('Company');
+		table.string('Posting Date');
+		table.string('Deadline');
+		table.string('Salary');
+		table.string('YOE');
+		table.string('Type');
+		table.string('Level');
+		table.string('Education');
+		table.string('Sex');
+		table.string('Career');
+		table.string('Age');
+		table.string('ID_Job');
+		table.string('Contact_with');
+		table.string('Location');
+		table.string('Note');
+		table.string('Phone_number');
+		table.string('Language');
+		table.string('Describe_job');
+		table.string('Benefits');
+		table.string('Skills');
+	  });
 };
 
 /**
@@ -24,5 +48,6 @@ exports.up = function(knex) {
  */
 exports.down = function(knex) {
 	return knex.schema
-	.dropTable('users')
+	.dropTable('users').dropTable('job_detail')
+	
 };
