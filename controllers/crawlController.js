416b2bbd999ea18cc7fcffb61f817eae8b97084d
CongTrinhh <nguyencongtrinhqb@gmail.com>
2024-01-03 19:32:45 +0700

    update api search

--- controllers/crawlController.js
+++ controllers/crawlController.js
@@ -6,7 +6,7 @@ const db = knex(config[environment]);
 
 const getDataCrawl = async () => {
   return await db("job_data").select(
-    "job_data.id",
+    "job_data.Id",
     "job_data.Title",
     "job_data.Company_Name",
     "job_data.Time",
@@ -27,7 +27,6 @@ const getDataCrawl = async () => {
     "job_data.Requirement",
     "job_data.Deadline",
     "job_data.Source_Picture"
-
   );
 };
 const getDataCrawlById = async (id) => {
@@ -36,9 +35,41 @@ const getDataCrawlById = async (id) => {
 const updateJob = (id, updatedJob) => {
   return db("job_data").where({ id }).update(updatedJob);
 };
+const getJobByTitle = async () => {
+  return await db("job_data")
+    .distinct("job_data.job")
+    .select("job_data.job")
+    .limit(10);
+};
+const filterJob = async (key1, key2, key3) => {
+  let cityKeyword = key3.toLowerCase();
+
+  if (cityKeyword === "hồ chí minh") {
+    cityKeyword = "hồ chí minh";
+  } else if (
+    cityKeyword === "tphcm" ||
+    cityKeyword === "tp.hcm" ||
+    cityKeyword === "hcm"
+  ) {
+    cityKeyword = "hồ chí minh";
+  }
+  console.log(cityKeyword);
+  return await db("job_data").whereRaw(
+    "LOWER(Title) like ?  AND LOWER(job) like ? AND (LOWER(City) like ? OR LOWER(City) like ? OR LOWER(City) like ?)",
+    [
+      `%${key1}%`,
+      `%${key2}%`,
+      `%${cityKeyword}%`,
+      `%tphcm%`,
+      `%hcm%`,
+    ]
+  );
+};
 
 module.exports = {
   getDataCrawl,
   getDataCrawlById,
   updateJob,
+  getJobByTitle,
+  filterJob,
 };
--- routes/datacrawl.js
+++ routes/datacrawl.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 router.use(bodyParser.json())
 
 router.get('/dataCrawl', async (req, res) => {
+	// const page = parseInt(req.query.page) || 1; 
+	// const pageSize = parseInt(req.query.pageSize) || 10;
 	try {
 		const data = await CrawlController.getDataCrawl();
 		res.json(data);
@@ -38,6 +40,34 @@ router.put('/updatejob/:id', async (req, res) => {
 		} else {
 			res.status(404).json({ error: 'Job not found' });
 		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: 'Internal server error' });
+	}
+})
+router.get('/jobtitle', async (req, res) => {
+	try {
+		const data = await CrawlController.getJobByTitle();
+		res.json({
+			message: 'Job Title',
+			data: data
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: 'Internal server error' });
+	}
+})
+router.get('/filterjob', async (req, res) => {
+	const key1 = req.query.key1 || "";
+	const key2 = req.query.key2 || "";
+	const key3 = req.query.key3 || "";
+	try {
+		const data = await CrawlController.filterJob(key1,key2,key3);
+		console.log("key1 = :",key1,"key2 = :",key2,"key3 = :",key3)
+		res.json({
+			message: 'Job Title',
+			data: data
+		});
 	} catch (err) {
 		console.error(err);
 		res.status(500).json({ error: 'Internal server error' });
